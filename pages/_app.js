0acdde2e6551b5cb316be989ab4fa8854c6d1bb6
RohalSaini <rohalsainia94@gmail.com>
2021-12-31 21:32:07 +0530

    nextjs

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+import Layout from '../components/Layout'
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+      <Component {...pageProps} />
+    </Layout>
+  ) 
+}
+
+export default MyApp
